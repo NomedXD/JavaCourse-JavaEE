33dbfbd489adbf207256214114f4ede6cce66e25
NomedXD <coovshow1@gmail.com>
2023-07-18 14:03:21 +0300

    hw_21072023 refactoring folders part 2

--- hw_02062023_task2/src/main/java/by/teachmeskills/task2/enums/PagesPathEnum.java
+++ hw_02062023_task2/src/main/java/by/teachmeskills/task2/enums/PagesPathEnum.java
@@ -1,14 +1,14 @@
 package by.teachmeskills.task2.enums;
 
 public enum PagesPathEnum {
-    SHOP_PAGE("shop.jsp"),
-    LOG_IN_PAGE("login.jsp"),
-    REGISTRATION_PAGE("register.jsp"),
-    CATEGORY_PAGE("category.jsp"),
-    CART_PAGE("cart.jsp"),
-    PRODUCT_PAGE("product.jsp"),
-    ACCOUNT_PAGE("account.jsp"),
-    SEARCH_PAGE("search.jsp");
+    SHOP_PAGE("/WEB-INF/view/shop.jsp"),
+    LOG_IN_PAGE("/WEB-INF/view/login.jsp"),
+    REGISTRATION_PAGE("/WEB-INF/view/register.jsp"),
+    CATEGORY_PAGE("/WEB-INF/view/category.jsp"),
+    CART_PAGE("/WEB-INF/view/cart.jsp"),
+    PRODUCT_PAGE("/WEB-INF/view/product.jsp"),
+    ACCOUNT_PAGE("/WEB-INF/view/account.jsp"),
+    SEARCH_PAGE("/WEB-INF/view/search.jsp");
 
     private final String path;
 
